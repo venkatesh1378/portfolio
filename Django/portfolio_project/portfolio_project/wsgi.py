2599b51871e7102fe89e24e9a49d34f1df939704
venkatesh1378 <103771081+venkatesh1378@users.noreply.github.com>
2025-05-12 19:40:19 +0530

    Update wsgi.py

--- Django/portfolio_project/portfolio_project/wsgi.py
+++ Django/portfolio_project/portfolio_project/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 application = get_wsgi_application()
